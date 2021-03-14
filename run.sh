f17b6b6b0330e2f6eaadbf948cbc3b1e054d8d50
Max Brauer <ma.brauer@live.de>
2021-03-14 10:22:12 +0100

    fix compression

--- run.sh
+++ run.sh
@@ -6,12 +6,12 @@ file=$2
 case $mode in
 	compress)
 		newFile="$(basename "$file" .decompressed).compressed"
-		/usr/src/RelaxHackathon.Compression/RelaxHackathon.Compression/bin/Release/net5.0/RelaxHackathon.Compression "$file" "${newFile}.temp"
-		xz --format=lzma2 --compress --keep --stdout "${newFile}.temp" > "$newFile"
-		rm "${newFile}.temp"
+		/usr/src/RelaxHackathon.Compression/RelaxHackathon.Compression/bin/Release/net5.0/RelaxHackathon.Compression \
+			"$file" "/usr/src/comp.temp"
+		xz --lzma2 --compress --keep --stdout "/usr/src/comp.temp" > "$newFile"
 		;;
 	decompress)
 		newFile="$(basename "$file" .compressed).decompressed"
-		xz --format=lzma2 --decompress --keep --stdout "${file}" > "$newFile"
+		xz --lzma2 --decompress --keep --stdout "${file}" > "$newFile"
 		;;
 esac
